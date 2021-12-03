2f7b5a216aaf71de2d16960fc861ae2b51a04d20
Godswill Azubike <43970405+Godswill-Azubike@users.noreply.github.com>
2021-12-03 14:25:30 +0100

    initial app commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter_plus/webview_flutter_plus.dart';
+
+void main() {
+  statusBarColor:
+  Colors.pink;
+  runApp(const WebViewPlusExample());
+}
+
+class WebViewPlusExample extends StatelessWidget {
+  const WebViewPlusExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          systemOverlayStyle:
+              SystemUiOverlayStyle(statusBarColor: Color(0xFFffb400)),
+        ),
+      ),
+      home: const WebViewPlusExampleMainPage(),
+    );
+  }
+}
+
+class WebViewPlusExampleMainPage extends StatefulWidget {
+  const WebViewPlusExampleMainPage({Key? key}) : super(key: key);
+
+  @override
+  _WebViewPlusExampleMainPageState createState() =>
+      _WebViewPlusExampleMainPageState();
+}
+
+class _WebViewPlusExampleMainPageState
+    extends State<WebViewPlusExampleMainPage> {
+  WebViewPlusController? _controller;
+  double _height = 1000;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          SizedBox(
+            height: _height,
+            child: WebViewPlus(
+              javascriptChannels: null,
+              initialUrl: 'https://globalfxpay.net/login',
+              onWebViewCreated: (controller) {
+                _controller = controller;
+              },
+              onPageFinished: (url) {
+                _controller?.getHeight().then((double height) {
+                  print("Height: " + height.toString());
+                  setState(() {
+                    _height = height;
+                  });
+                });
+              },
+              javascriptMode: JavascriptMode.unrestricted,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
